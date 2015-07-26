d7f98e2ef1a1b3dc3eced9dc2dfe131e3b6df083
Hobson Lane <hobsonlane@gmail.com>
2015-07-25 22:35:59 -0700

    home app and urls to link to it

---
+++ totalgood/home/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', 'home.views.index', name='home'),
+]
--- totalgood/totalgood/urls.py
+++ totalgood/totalgood/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import conceive.urls
+
+
 urlpatterns = [
     # Examples:
-    url(r'^$', 'totalgood.views.home', name='home'),
+    url(r'^', 'home.views.index', name='home'),
+    # url(r'^decoder/', include(decoder.urls)),
+    url(r'^conceive/', include(conceive.urls)),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
