4380a70304be9762f1884abfa2802e8ee4668b7e
Hobson Lane <hobsonlane@gmail.com>
2015-10-04 16:35:35 -0700

    pacs urls instead of conceive.urls

--- totalgood/totalgood/urls.py
+++ totalgood/totalgood/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-import conceive.urls
+import pacs.urls
 
 
 urlpatterns = [
     # Examples:
     url(r'^', 'home.views.index', name='home'),
     # url(r'^decoder/', include(decoder.urls)),
-    url(r'^conceive/', include(conceive.urls)),
+    url(r'^pacs/', include(pacs.urls)),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
