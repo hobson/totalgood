9c9f8ace8d5301eaedcea38ff53402b6d42fc76d
Hobson Lane <hobsonlane@gmail.com>
2015-10-04 17:05:53 -0700

    get rid of extra manage.py

--- totalgood/manage.py
+++ totalgood/manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+# PROJECT_NAME = os.path.basename(os.path.dirname(__file__))
+# PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), PROJECT_NAME))
+
+# sys.path.insert(0, PROJECT_ROOT)
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "totalgood.settings")
 
